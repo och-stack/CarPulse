b29afa3b749d0d77287642e803d50d6564ebe160
Chuu Harn <ochuuharn@gmail.com>
2026-09-15 11:07:29 +0800

    update dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -121,7 +121,7 @@ function Dashboard() {
                         </Button>
                     </div>
 
-                    <div className="row g-1 justify-content-start">
+                    <div className="row gx-1 gy-1 justify-content-start">
                         {filteredDevices.map(device => (
                             <div
                                 key={device.id}
